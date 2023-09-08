9a76cd913a079da0fa07aa88031b696b740fbb1e
Diego Feldman <diegofeldman@gmail.com>
2023-09-08 17:51:33 -0300

    nuevo

---
+++ Pagina 1/js/script.js
@@ -0,0 +1,26 @@
+ var nombre = "Diego"; //la asignación de la variable es con el =
+ var telefono = 1153786359; //sin comillas si es una variable numérica, con comillas si es string
+ var suscriptoONo = true;
+ var email = "diegofeldman@gmail.com";
+
+ //El método alert() muestra un mensaje por pantalla como un popup
+ //alert("Hola " + nombre);
+
+//El objeto "console" sirve para usar métodos/funciones que realicen cosas en la consola (se accede a través de la opción Inspeccionar)
+ //La función console.log() sirve para mostrar en la consola (accediento por la opción de Inspeccionar) lo que le incluyamos entre paréntesis
+console.log("Nombre: " + nombre + " " + typeof nombre);
+console.log("Teléfono: " + telefono + " " + typeof telefono);
+
+/*
+La función "prompt" sirve para pedir datos por pantalla
+
+var color = prompt("Escribí un color primario");
+*/
+
+/*
+Se pueden combinar las etiquetas con las clases, como abajo, que usa la etiqueta "div" junto con la clase ".especial" y junto a otra etiqueta "p"
+
+div.especial p {
+
+}
+*/
